ad0434f42314b491eaea3820558a6a488d6451be
Sjoerd Job Postmus <sj.postmus@developers.nl>
2016-05-09 18:33:08 +0200

    Better formatting for lessons in edit overview.

--- lessons/models.py
+++ lessons/models.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 from taggit.managers import TaggableManager
 
 
+@python_2_unicode_compatible
 class Lesson(models.Model):
     class Meta:
         ordering = ('-modified_at',)
@@ -14,3 +16,6 @@ class Lesson(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
